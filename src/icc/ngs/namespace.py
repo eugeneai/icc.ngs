5a64778a662598ad6eca9961d43a1b68733ecc92
Evgeny Cherkashin <eugeneai@irnok.net>
2018-09-07 01:16:43 +0800

    Added useful namespaces.

--- src/icc/ngs/namespace.py
+++ src/icc/ngs/namespace.py
@@ -1,3 +1,7 @@
 from rdflib.namespace import Namespace
 NGS = Namespace('http://icc.ru/ontologies/NGS/')
 NGSP = Namespace('http://icc.ru/ontologies/NGS/processing/')
+V = Namespace('http://www.w3.org/2006/vcard/ns#')
+SCHEMA = Namespace('http://schema.org/')
+OSLC = Namespace('http://open-services.net/ns/core#')
+CNT = Namespace('http://www.w3.org/2011/content#')
